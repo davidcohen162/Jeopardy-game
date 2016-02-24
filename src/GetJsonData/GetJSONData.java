e2e9135ad6a7eb1d3fbbc6bddd89320621d58527
Yehuda globerman <ylgloberman@gmail.com>
2016-02-23 19:20:29 -0500

    Changed many methods

--- src/GetJsonData/GetJSONData.java
+++ src/GetJsonData/GetJSONData.java
@@ -4,35 +4,36 @@ import okhttp3.*;
 
 import java.io.IOException;
 import java.util.HashMap;
-
+import java.util.Set;
 
 public class GetJSONData {
 
 
 //    The base url of the api, in this case http://jservice.io/api/
-    private String urlString;
+    private String baseUrlString;
     //Add query parameters here, as needed.
-    private HashMap<String, Integer> parameters;
+    protected HashMap<String, Integer> parameters;
+    private static OkHttpClient client = new OkHttpClient();
     private String jsonResponse;
 
     public GetJSONData(String urlString) {
-        this.urlString = urlString;
+        this.baseUrlString = urlString;
+        parameters = new HashMap<>();
     }
 
-    public String getUrlString() {
-        return urlString;
+    public String getBaseUrlString() {
+        return baseUrlString;
     }
 
     public void setUrl(String urlString) {
-        this.urlString = urlString;
+        this.baseUrlString = urlString;
     }
 
 
     public void getJsonFromWeb () throws IOException {
-        OkHttpClient client = new OkHttpClient();
 
         Request request = new Request.Builder()
-                .url(this.urlString)
+                .url(this.baseUrlString)
                 .build();
 
         Response response = client.newCall(request).execute();
@@ -44,12 +45,41 @@ public class GetJSONData {
         }
     }
 
-    public void addParameterQuery (String queryName, int value) {
+    public void getJsonFromWebWithQueries(Set<String> keys) throws IOException {
+        String parameters = "?";
+        for(String key : keys)
+        {
+            if (this.parameters.containsKey(key))
+            {
+                parameters += key + "=" + this.parameters.get(key) + "&";
+            } else {
+                throw new RuntimeException("Parameter not set.");
+            }
+        }
+        Request request = new Request.Builder()
+                .url(this.baseUrlString + parameters)
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        if (!response.isSuccessful()) {
+            throw new IOException("Unexpected code " + response);
+        } else {
+            this.jsonResponse = response.body().string();
+        }
+    }
+
+    protected void addParameterQuery (String queryName, int value) {
         parameters.put(queryName, value);
     }
 
+    protected void removeQueryParameter(String queryName)
+    {
+        parameters.remove(queryName);
+    }
 
-    public String getJsonResponse() {
+
+    public String getLastJsonResponse() {
         return this.jsonResponse;
     }
 
--- src/GetJsonData/RandomQuestionsJson.java
+++ src/GetJsonData/RandomQuestionsJson.java
@@ -4,27 +4,24 @@ import java.io.IOException;
 
 
 public class RandomQuestionsJson extends GetJSONData{
+    String countQuery = "count";
 
-    int count;
     public RandomQuestionsJson() {
         super("http://jservice.io/api/random?");
     }
 
 
-    public int getCount() {
-        return count;
-    }
-
 //    limited to 100
-    public void setCount(int count) {
-        this.count = count;
+    public void setCountParameter(int count) {
+        addParameterQuery(countQuery, count);
     }
 
     public void getOneRandomCluesFromWeb() throws IOException {
         getJsonFromWeb();
     }
 
+//    Assumes count was set
     public void getRandomCluesWithCount() throws IOException {
-        setUrl(getUrlString() +  + count);
+        getJsonFromWebWithQueries(parameters.keySet());
     }
 }
