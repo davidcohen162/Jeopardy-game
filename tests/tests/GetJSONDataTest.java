c4def22a3a00cf38a0aedbe79b392063ac34633b
Yehuda globerman <ylgloberman@gmail.com>
2016-03-08 17:48:41 -0500

    Added a few tests, changed some others

--- tests/tests/GetJSONDataTest.java
+++ tests/tests/GetJSONDataTest.java
@@ -6,7 +6,9 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Scanner;
 
 import static org.junit.Assert.*;
@@ -15,92 +17,118 @@ import static org.junit.Assert.*;
 public class GetJSONDataTest {
 
     private GetJSONData JsonData;
-    private String baseUrlOfAPI = "http://jservice.io/api/category";
-    private static String jsonForCategory5 = "";
+    private String baseUrlOfAPI = "http://jservice.io/api/categories";
+    private static String jsonToTestAgainst = "";
 
     @BeforeClass
     public static void setJsonForCategory5() throws FileNotFoundException {
-        java.io.File file = new java.io.File("D:\\My Documents\\Code\\IDEA Projects\\Jeopardy\\Jeopardy-game\\JsonForCategory5.json");
+        java.io.File file = new java.io.File("D:\\My Documents\\Code\\IDEA Projects\\Jeopardy\\Jeopardy-game\\jsonToTestAgainst.json");
         Scanner input = new Scanner(file);
 
         if (!input.hasNext()) {
-            jsonForCategory5 = null;
+            jsonToTestAgainst = null;
         } else {
-            jsonForCategory5 += input.next();
+            jsonToTestAgainst += input.next();
             while (input.hasNext()) {
-                jsonForCategory5 += " " + input.next();
+                jsonToTestAgainst += " " + input.next();
             }
         }
         input.close();
     }
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         JsonData = new GetJSONData(baseUrlOfAPI);
     }
 
     @Test
-    public void testGetBaseUrlString() throws Exception {
+    public void testGetBaseUrlString() {
         assertEquals(baseUrlOfAPI, JsonData.getBaseUrlString());
     }
 
     @Test
-    public void testSetBaseUrlStringUrl() throws Exception {
+    public void testSetBaseUrlStringUrl() {
         String newBaseUrl = "http://jservice.io/api/random";
         JsonData.setBaseUrlStringUrl(newBaseUrl);
         assertEquals(newBaseUrl, JsonData.getBaseUrlString());
     }
 
     @Test
-    public void testRequestJsonFromWeb() throws Exception {
-        JsonData.setBaseUrlStringUrl("http://jservice.io/api/category?id=5");
+    public void testRequestJsonFromWeb() throws IOException {
+        JsonData.setBaseUrlStringUrl("http://jservice.io/api/categories?count=50");
         JsonData.requestJsonFromWeb();
-        assertEquals(jsonForCategory5, JsonData.getLastJsonResponse());
+        assertEquals(jsonToTestAgainst, JsonData.getLastJsonResponse());
     }
 
     @Test
-    public void testRequestJsonFromWebWithQueries() throws Exception {
-        JsonData.addParameterQuery("id", "5");
+    public void testRequestJsonFromWebWithQueries() throws IOException {
+        JsonData.addParameterQuery("count", "50");
         JsonData.requestJsonFromWebWithQueries(JsonData.getParametersMap().keySet());
 
-        assertEquals(jsonForCategory5, JsonData.getLastJsonResponse());
+        assertEquals(jsonToTestAgainst, JsonData.getLastJsonResponse());
     }
 
     @Test
-    public void testAddParameterQuery() throws Exception {
-        JsonData.addParameterQuery("id", "5");
+    public void RequestWithQueriesIgnoresParametersNotPassedByKeyset() throws IOException {
+        JsonData.addParameterQuery("count", "50");
+        JsonData.addParameterQuery("offset", "100");
 
-        assertTrue(JsonData.getParametersMap().containsKey("id"));
-        assertEquals("5", JsonData.getParametersMap().get("id"));
+        HashSet<String> set = new HashSet<>();
+        set.add("count");
+
+        JsonData.requestJsonFromWebWithQueries(set);
+        assertEquals(jsonToTestAgainst, JsonData.getLastJsonResponse());
     }
 
     @Test
-    public void testRemoveQueryParameter() throws Exception {
-        JsonData.addParameterQuery("id", "5");
-        JsonData.removeQueryParameter("id");
+    public void testAddParameterQuery() {
+        JsonData.addParameterQuery("count", "50");
 
-        assertFalse(JsonData.getParametersMap().containsKey("id"));
+        assertTrue(JsonData.getParametersMap().containsKey("count"));
+        assertEquals("50", JsonData.getParametersMap().get("count"));
     }
 
     @Test
-    public void testGetQueryValue() throws Exception {
-        JsonData.addParameterQuery("id", "5");
-        assertEquals("5", JsonData.getParametersMap().get("id"));
+    public void testRemoveQueryParameter() {
+        JsonData.addParameterQuery("count", "50");
+        JsonData.removeQueryParameter("count");
+
+        assertFalse(JsonData.getParametersMap().containsKey("count"));
     }
 
     @Test
-    public void testGetParametersMap() throws Exception {
-        JsonData.addParameterQuery("id", "5");
+    public void testGetQueryValue() {
+        JsonData.addParameterQuery("count", "50");
+        assertEquals("50", JsonData.getParametersMap().get("count"));
+    }
+
+    @Test
+    public void testGetParametersMap() {
+        JsonData.addParameterQuery("count", "50");
         HashMap<String, String> parametersClone = new HashMap<>();
-        parametersClone.put("id", "5");
+        parametersClone.put("count", "50");
 
         assertEquals(parametersClone, JsonData.getParametersMap());
     }
 
     @Test
-    public void testGetLastJsonResponse() throws Exception {
-        JsonData.setBaseUrlStringUrl("http://jservice.io/api/category?id=5");
+    public void testGetLastJsonResponse() throws IOException {
+        JsonData.setBaseUrlStringUrl("http://jservice.io/api/categories?count=50");
         JsonData.requestJsonFromWeb();
-        assertEquals(jsonForCategory5, JsonData.getLastJsonResponse());
+        assertEquals(jsonToTestAgainst, JsonData.getLastJsonResponse());
+    }
+
+    @Test
+    public void LastJsonResponseStartsNull() {
+        assertNull(JsonData.getLastJsonResponse());
+    }
+
+    @Test
+    public void LastJsonResponseStringGetsUpdatedFollowingANewRequest() throws IOException {
+        JsonData.setBaseUrlStringUrl("http://jservice.io/api/random");
+        JsonData.requestJsonFromWeb();
+        String oldJson = JsonData.getLastJsonResponse();
+        JsonData.requestJsonFromWeb();
+        assertNotEquals(oldJson, JsonData.getLastJsonResponse());
     }
 }
