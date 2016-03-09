90cb04e1d409984c97632fe6c0dbf1467bc9c6eb
Yehuda globerman <ylgloberman@gmail.com>
2016-03-08 19:52:31 -0500

    Wrote some basic tests for CategoriesJson

--- src/GetJsonData/CategoriesJson.java
+++ src/GetJsonData/CategoriesJson.java
@@ -5,27 +5,23 @@ import Questions.Category;
 
 import java.io.IOException;
 
-public class CategoriesJson extends GetJSONData{
+public class CategoriesJson extends GetJSONData {
 
     public static final int MAX_OFFSET = 18416;
-//    The maximum categories you can request in one request.
-public static final int MAX_CATEGORIES_REQUEST = 100;
-//      countQuery: an optional parameter to request more than 1 category at a time. Currently limited to 100
+    //    The maximum categories you can request in one request.
+    public static final int MAX_CATEGORIES_REQUEST = 100;
+    //      countQuery: an optional parameter to request more than 1 category at a time. Currently limited to 100
     private static final String countQuery = "count";
-//    By default the request will always return the same categories starting from x. Setting the Offset will return
+    //    By default the request will always return the same categories starting from x. Setting the Offset will return
 //    categories starting from the offset number.
-    public static final String offsetQuery = "offset";
+    private static final String offsetQuery = "offset";
 
 
     public CategoriesJson() {
         super("http://jservice.io/api/categories");
     }
 
-    public void setCountAmount(int count) {
-        addParameterQuery(CategoriesJson.countQuery, Integer.toString(count));
-    }
-
-    public int getCountAmount(){
+    public int getCountAmount() {
         try {
             return Integer.parseInt(getQueryValue(CategoriesJson.countQuery));
         } catch (RuntimeException e) {
@@ -33,8 +29,8 @@ public static final int MAX_CATEGORIES_REQUEST = 100;
         }
     }
 
-    public void setOffsetAmount(int offsetAmount) {
-        addParameterQuery(CategoriesJson.countQuery, Integer.toString(offsetAmount));
+    private void setCountAmount(int count) {
+        addParameterQuery(CategoriesJson.countQuery, Integer.toString(count));
     }
 
     public int getOffsetAmount() {
@@ -45,7 +41,11 @@ public static final int MAX_CATEGORIES_REQUEST = 100;
         }
     }
 
-    public void requestCategoriesJsonFromWeb (int count, int offSet) throws IOException {
+    private void setOffsetAmount(int offsetAmount) {
+        addParameterQuery(CategoriesJson.offsetQuery, Integer.toString(offsetAmount));
+    }
+
+    public void requestCategoriesJsonFromWeb(int count, int offSet) throws IOException {
         setCountAmount(count);
         setOffsetAmount(offSet);
 
@@ -55,6 +55,6 @@ public static final int MAX_CATEGORIES_REQUEST = 100;
     public Category[] getArrayOfCategories(int count, int offSet) throws IOException {
         requestCategoriesJsonFromWeb(count, offSet);
         Category[] categories = gson.fromJson(getLastJsonResponse(), Category[].class);
-        return  categories;
+        return categories;
     }
 }
