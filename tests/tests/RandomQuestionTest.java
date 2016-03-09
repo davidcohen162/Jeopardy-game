5a7c3693302175ff10b6df50fd19ee284714b524
Yehuda globerman <ylgloberman@gmail.com>
2016-03-08 23:24:20 -0500

    Wrote some basic tests for Question class

--- tests/tests/RandomQuestionTest.java
+++ tests/tests/RandomQuestionTest.java
@@ -1,12 +1,21 @@
 package tests;
 
+import GetJsonData.RandomQuestionsJson;
+import Questions.RandomQuestion;
 import org.junit.Before;
+
+import java.io.IOException;
 
 
 public class RandomQuestionTest {
 
-    @Before
-    public void setUp() throws Exception {
+    private RandomQuestion question;
+    private RandomQuestionsJson questionsJson;
 
+    @Before
+    public void setUp() throws IOException {
+        questionsJson = new RandomQuestionsJson();
+        question = new RandomQuestion(questionsJson.getRandomClueObject());
     }
+
 }
