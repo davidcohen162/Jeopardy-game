25d649e1206a7187d4105e9b381756e33dd93e3d
Yehuda globerman <ylgloberman@gmail.com>
2016-03-13 14:56:03 -0400

    Added some more tests for CategoriesJson

--- tests/tests/CategoriesJsonTest.java
+++ tests/tests/CategoriesJsonTest.java
@@ -6,9 +6,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 
 public class CategoriesJsonTest {
@@ -62,4 +62,47 @@ public class CategoriesJsonTest {
             assertNotNull(c);
         }
     }
+
+    @Test
+    public void testGetArrayOfRandomCats() throws IOException {
+        Category[] cats = categoriesJson.getArrayOfRandomCats(1);
+        assertEquals(1, cats.length);
+    }
+
+    @Test
+    public void testGetArrayOfRandomCatsWithZero() throws IOException {
+        Category[] cats = categoriesJson.getArrayOfRandomCats(0);
+        assertEquals(0, cats.length);
+    }
+
+    @Test
+    public void testGetArrayOfRandomCatsWithMoreThan1() throws IOException {
+        Category[] cats = categoriesJson.getArrayOfRandomCats(5);
+        assertEquals(5, cats.length);
+    }
+
+    @Test
+    public void testGetArrayListOfCatsWithMoreThan_n_Clues() throws IOException {
+        ArrayList<Category> cats = categoriesJson.getArrayListOfCatsWithMoreThan_n_Clues(1, 20);
+        assertEquals(1, cats.size());
+        for (Category c : cats) {
+            assertTrue(c.getCluesCount() >= 20);
+        }
+    }
+
+    @Test
+    public void TestGetArrayListOfCatsWithMoreThan_n_CluesWhenRequesting5Clues() throws IOException {
+        ArrayList<Category> cats = categoriesJson.getArrayListOfCatsWithMoreThan_n_Clues(5, 20);
+        assertEquals(5, cats.size());
+        for (Category c : cats) {
+            assertTrue(c.getCluesCount() >= 20);
+        }
+    }
+
+    @Test
+    public void TestGetArrayListOfCatsWithMoreThan_n_CluesWhenRequesting0Clues() throws IOException {
+        ArrayList<Category> cats = categoriesJson.getArrayListOfCatsWithMoreThan_n_Clues(0, 20);
+        assertEquals(0, cats.size());
+    }
+
 }
