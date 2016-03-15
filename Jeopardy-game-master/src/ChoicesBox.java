0b1d82b31118334f7950024c7d2f800987f0b537
David Cohen <David Cohen@DAVIDCOHENPC.home>
2016-03-15 04:48:52 -0400

    Added an OK button

--- Jeopardy-game-master/src/ChoicesBox.java
+++ Jeopardy-game-master/src/ChoicesBox.java
@@ -12,8 +12,10 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-public class ChoicesBox  {    
+public class ChoicesBox  {  
+	
 	public static void show(String message, String title) throws IOException     { 
+		
 		//Creating an object of imported question class
 		Stage stage = new Stage();   //Setting the stage   
 		stage.initModality(Modality.APPLICATION_MODAL);        
@@ -25,6 +27,12 @@ public class ChoicesBox  {
 		GameInRandomCategory gameInRandomCategory = new GameInRandomCategory(questionsToAsk);
         gameInRandomCategory.setUpQuestions();
 		ComboBox<String> cbox=new ComboBox<String>();//Setting the combo box
+		cbox.setValue(cbox.getValue());
+		Button btnOK = new Button("OK");      //Creating a button  
+		btnOK.setMinWidth(75);        
+		btnOK.setOnAction(e -> //Setting the OK button to continue the game
+				stage.close());
+		 
 		for(int i=0;i<questionsToAsk;i++){//Setting the question text
 			qlbl.setText("Question: " + (i + 1) + " out of: " + gameInRandomCategory.getAmountOfQuestionsInGame() +":"+gameInRandomCategory.getQuestionString(i)+
                     "\t\tScore: " + gameInRandomCategory.getScore()
@@ -37,12 +45,13 @@ public class ChoicesBox  {
 			 		cbox.getItems().add(gameInRandomCategory.getMultipleChoiceAnswers(i).get(j));//Adding clues to combobox
 		
 		 		}
-		 		cbox.setValue(cbox.getValue());
+		 		
 		 		String input=cbox.getValue();
 		 		int choiceIndex=0;
 		 		for(int h=0;h<gameInRandomCategory.getMultipleChoiceAnswers(i).size();h++){
 		 			if((gameInRandomCategory.getMultipleChoiceAnswers(i).get(h).equals(input))){
 		 				choiceIndex=h;
+		 				break;
 		 			}
 		 		}
 		 		gameInRandomCategory.setPlayersAnswer(i, gameInRandomCategory.getMultipleChoiceAnswers(i).get(choiceIndex));
@@ -53,7 +62,7 @@ public class ChoicesBox  {
 	        pane.setHgap(10);
 	        pane.add(qlbl, 0, 1);//Question text and score
 	        pane.add(cbox,0,4); //Adding combobox to pane
-	        
+	        pane.add(btnOK, 0, 7);//Adding the OK button
 	        
 			pane.setAlignment(Pos.CENTER); //Setting combobox in center
 		    Scene scene = new Scene(pane);     //Adding pane to scene    
