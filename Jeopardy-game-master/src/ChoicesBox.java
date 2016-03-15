c7bf629a601713bba1b91f765da30b06af985674
David Cohen <David Cohen@DAVIDCOHENPC.home>
2016-03-15 05:39:04 -0400

    Added a stop button to the dialog box which will stop the game and show the end score

--- Jeopardy-game-master/src/ChoicesBox.java
+++ Jeopardy-game-master/src/ChoicesBox.java
@@ -2,6 +2,9 @@
 import java.io.IOException;
 
 import Game.GameInRandomCategory;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -22,20 +25,30 @@ public class ChoicesBox  {
 		stage.setTitle(title);   //The title parameter     
 		stage.setMinWidth(650);//The minimum width
 		Label qlbl = new Label();//The question label
-		
+		Label slbl=new Label();
 		int questionsToAsk=10;
 		GameInRandomCategory gameInRandomCategory = new GameInRandomCategory(questionsToAsk);
         gameInRandomCategory.setUpQuestions();
 		ComboBox<String> cbox=new ComboBox<String>();//Setting the combo box
 		cbox.setValue(cbox.getValue());
-		Button btnOK = new Button("OK");      //Creating a button  
+		Button btnOK = new Button("OK");      //Creating an OK button  
 		btnOK.setMinWidth(75);        
 		btnOK.setOnAction(e -> //Setting the OK button to continue the game
 				stage.close());
-		 
+		 Button btnStop=new Button("Stop");//Adding a stop button
+		 btnStop.setMinWidth(75);
+		 btnStop.setOnAction(new EventHandler<ActionEvent>() {//Assigning actions to stop button
+			    public void handle(ActionEvent e) {
+			    	MessageBox.show("Game Over"+"\nScore is"+ "\t\tScore: " + gameInRandomCategory.getScore()
+                    + " out of: " + gameInRandomCategory.getMaxScore(), "Game Over");
+			        System.exit(0);
+			    }
+			});
+
 		for(int i=0;i<questionsToAsk;i++){//Setting the question text
-			qlbl.setText("Question: " + (i + 1) + " out of: " + gameInRandomCategory.getAmountOfQuestionsInGame() +":"+gameInRandomCategory.getQuestionString(i)+
-                    "\t\tScore: " + gameInRandomCategory.getScore()
+			qlbl.setText("Question: " + (i + 1) + " out of: " + gameInRandomCategory.getAmountOfQuestionsInGame() +":"+gameInRandomCategory.getQuestionString(i)
+                   );
+			slbl.setText( "\t\tScore: " + gameInRandomCategory.getScore()
                     + " out of: " + gameInRandomCategory.getMaxScore());
 			cbox.setPromptText(gameInRandomCategory.getMultipleChoiceAnswers(i).get(0));
 
@@ -58,20 +71,22 @@ public class ChoicesBox  {
 		 		
 		 		
 		    GridPane pane = new GridPane();  //Pane to place the cbox in 
-		    pane.setVgap(4);
+		    pane.setVgap(10);
 	        pane.setHgap(10);
 	        pane.add(qlbl, 0, 1);//Question text and score
-	        pane.add(cbox,0,4); //Adding combobox to pane
-	        pane.add(btnOK, 0, 7);//Adding the OK button
-	        
+	        pane.add(slbl, 1, 1);
+	        pane.add(cbox,0,2); //Adding combobox to pane
+	        pane.add(btnOK,1, 2);//Adding the OK button
+	        pane.add(btnStop, 2, 2);
 			pane.setAlignment(Pos.CENTER); //Setting combobox in center
+			pane.setPadding(new Insets(10,10,10,10));
 		    Scene scene = new Scene(pane);     //Adding pane to scene    
 			stage.setScene(scene);       //Adding scene to stage 	
 			stage.showAndWait();//Showing in window
 			
 			if (gameInRandomCategory.aQuestionWasAnsweredCorrectly(i)) {
 	 			
-                MessageBox.show("Excelsior!", ""+(i+1)+"out of " + gameInRandomCategory.getAmountOfQuestionsInGame());
+                MessageBox.show("Excelsior!", ""+(i+1)+" out of " + gameInRandomCategory.getAmountOfQuestionsInGame());
             } else {
             	MessageBox.show("Drink Coke, play again :; ", ""+(i+1)+" out of " + gameInRandomCategory.getAmountOfQuestionsInGame());
             }
