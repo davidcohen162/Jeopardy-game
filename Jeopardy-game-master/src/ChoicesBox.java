84b128d763a7df8ffe42857d5e1ee2c3498d9c63
David Cohen <David Cohen@DAVIDCOHENPC.home>
2016-03-16 17:07:19 -0400

    UI Improvements Combo box changed to radio buttons Font size increased for choices,OK, and Quit Game buttons All elements use Verdana 14 font

--- Jeopardy-game-master/src/ChoicesBox.java
+++ Jeopardy-game-master/src/ChoicesBox.java
@@ -13,6 +13,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
@@ -31,14 +33,23 @@ public class ChoicesBox  {
 		Label slbl=new Label();//The score label
 		int questionsToAsk=10;
 		GameInRandomCategory gameInRandomCategory = new GameInRandomCategory(questionsToAsk);
-        gameInRandomCategory.setUpQuestions();
-		ComboBox<String> cbox=new ComboBox<String>();//Setting the combo box
+        gameInRandomCategory.setUpQuestions();//The radio buttons
+		RadioButton rb1=new RadioButton();
+		RadioButton rb2=new RadioButton();
+		RadioButton rb3=new RadioButton();
+		RadioButton rb4=new RadioButton();
+		rb1.setFont(Font.font("Verdana",14));
+		rb2.setFont(Font.font("Verdana",14));
+		rb3.setFont(Font.font("Verdana",14));
+		rb4.setFont(Font.font("Verdana",14));
+
 		Button btnOK = new Button("OK");      //Creating an OK button  
-		btnOK.setMinWidth(75);        
-		
-		 Button btnStop=new Button("Quit Game");//Adding a stop button
+		btnOK.setMinWidth(75); 
+		btnOK.setFont(Font.font("Verdana",14));
+		Button btnStop=new Button("Quit Game");//Adding a stop button
 		 btnStop.setMinWidth(75);
-		 btnStop.setOnAction(new EventHandler<ActionEvent>() {//Assigning actions to stop button
+		btnStop.setFont(Font.font("Verdana",14));
+		btnStop.setOnAction(new EventHandler<ActionEvent>() {//Assigning actions to stop button
 			    public void handle(ActionEvent e) {
 			    	MessageBox.show("Game Over"+""+"\t\tScore: " + gameInRandomCategory.getScore()
                     + " out of: " + gameInRandomCategory.getMaxScore(), "Game Over");
@@ -54,20 +65,37 @@ public class ChoicesBox  {
 			slbl.setFont(Font.font("Verdana", 14));
 			slbl.setText( "\t\tScore: " + gameInRandomCategory.getScore()
                     + " out of: " + gameInRandomCategory.getMaxScore());
-			cbox.getItems().clear(); //Clearing the combo box array
+			
+					final ToggleGroup group = new ToggleGroup();
+			 		rb1.setText(gameInRandomCategory.getMultipleChoiceAnswers(i).get(0));//Adding clues to combobox
+			 		rb2.setText(gameInRandomCategory.getMultipleChoiceAnswers(i).get(0+1));//Adding clues to combobox
+			 		rb3.setText(gameInRandomCategory.getMultipleChoiceAnswers(i).get(0+2));//Adding clues to combobox
+			 		rb4.setText(gameInRandomCategory.getMultipleChoiceAnswers(i).get(0+3));//Adding clues to combobox
+			 		rb1.setToggleGroup(group);
+			 		rb2.setToggleGroup(group);
+			 		rb3.setToggleGroup(group);
+			 		rb4.setToggleGroup(group);
+			 		rb1.setSelected(true);
 
-		 		for (int j = 0; j < gameInRandomCategory.getMultipleChoiceAnswers(i).size(); j++) {
-			 		cbox.getItems().add(gameInRandomCategory.getMultipleChoiceAnswers(i).get(j));//Adding clues to combobox
-		
-		 		}
+		 		
 	 			int question=i;
 
 		 		btnOK.setOnAction(new EventHandler<ActionEvent>(){
 					//Setting the OK button to continue the game and get user input
 					//The OK button must be pressed or first choice in the box will be auto selected
 		 			public void handle(ActionEvent e){
-						String selected=cbox.getValue();
-						
+		 				String selected="";
+		 				if(rb1.isSelected()){
+		 					selected=rb1.getText();}
+		 				else if(rb2.isSelected()){
+		 					selected=rb2.getText();
+		 				}
+		 				else if(rb3.isSelected()){
+		 					selected=rb3.getText();
+		 				}
+		 				else if(rb4.isSelected()){
+		 					selected=rb4.getText();
+		 				}
 					 		gameInRandomCategory.setPlayersAnswer(question, selected);
 					 		if(question==9){
 					 		MessageBox.show("Final score is: " + "\t\tScore: " + gameInRandomCategory.getScore()
@@ -79,18 +107,21 @@ public class ChoicesBox  {
 					}
 				);
 		 		
-		      cbox.setValue(gameInRandomCategory.getMultipleChoiceAnswers(i).get(0));
+		      
 		 		
 		 		
-		    GridPane pane = new GridPane();  //Pane to place the cbox in 
+		    GridPane pane = new GridPane();  //Pane to place the radio buttons in 
 		    pane.setVgap(10);
 	        pane.setHgap(10);
 	        pane.add(qlbl, 0, 1);//Question text and score
 	        pane.add(slbl, 2, 1);
-	        pane.add(cbox,0,2); //Adding combobox to pane
-	        pane.add(btnOK,1,2);//Adding the OK button
-	        pane.add(btnStop, 2,2);
-			pane.setAlignment(Pos.CENTER); //Setting combobox in center
+	        pane.add(rb1,0,2); //Adding radio buttons to pane
+	        pane.add(rb2, 0, 3);
+	        pane.add(rb3, 0, 4);
+	        pane.add(rb4, 0, 5);
+	        pane.add(btnOK,1,5);//Adding the OK button
+	        pane.add(btnStop, 2,5);
+			pane.setAlignment(Pos.CENTER); //Setting radio buttons in center
 			pane.setPadding(new Insets(10,10,10,10));//Setting window chrome around contents
 		    Scene scene = new Scene(pane);     //Adding pane to scene    
 			stage.setScene(scene);       //Adding scene to stage 
